32168f792c09cb14d7ae00e59efb8bb5df9bd708
ompangi <ce19f003@naver.com>
2021-01-19 19:46:11 +0900

    Initial commit

---
+++ src/main/java/cbu/CBUMS/CbumsApplication.java
@@ -0,0 +1,13 @@
+package cbu.CBUMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CbumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CbumsApplication.class, args);
+	}
+
+}
